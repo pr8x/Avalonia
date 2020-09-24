17176022e4eda0c79b8a71f97e6ad5e3b22a6fce
Luis von der Eltz <luis@gritworld.com>
2020-09-24 15:53:23 +0200

    .wew

---
+++ samples/Sandbox/FancyControl.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace Sandbox
+{
+    public interface IItem : IControl
+    {
+    }
+
+    public class ItemBase<T> : Control, IItem
+    {
+        public static readonly StyledProperty<bool> XProperty =
+            AvaloniaProperty.Register<FancyControl, bool>(nameof(Item));
+
+        public bool X
+        {
+            get => GetValue(XProperty);
+            set => SetValue(XProperty, value);
+        }
+    };
+
+    public class Item : ItemBase<bool>
+    {
+
+    }
+
+    public class FancyControl : Control
+    {
+        public static readonly StyledProperty<IItem> ItemProperty =
+            AvaloniaProperty.Register<FancyControl, IItem>(nameof(Item));
+
+        public IItem Item
+        {
+            get => GetValue(ItemProperty);
+            set => SetValue(ItemProperty, value);
+        }
+    }
+}
